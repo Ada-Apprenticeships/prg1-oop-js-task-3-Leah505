bac2d997d8de4b147f61cf6b9fce83eaa61c877c
Leah505 <leah.clarke@ada.ac.uk>
2024-10-28 12:58:09 +0000

    First Go

--- oop.js
+++ oop.js
@@ -1,14 +1,21 @@
 PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 
-function validInteger (value) { // value can be a string or a number (integer)
+function validInteger (value) {
+      // Check if value is a number or a string that can be converted to an integer
+      const parsed = parseInt(value);
   
 }  
 
 
-function validatePriority(priority) { // value can be a string or a number (integer)
-  
-}
+function validatePriority(priority) {
+  const validPriorities = Object.values(PRIORITY);
+  const parsedPriority = parseInt(priority, 10);
+  if (validPriorities.includes(parsedPriority)){
+    return parsedPriority} else {
+      return PRIORITY.LOW;
+    }
+  }
 
 
 function todaysDate () {
@@ -17,12 +24,37 @@ function todaysDate () {
 
 
 class Task  {
+  #added;
+  #title;
+  #priority;
 
-  // (title, priority)
+  constructor(added, title, priority) {
+    this.#added = todaysDate();
+    this.#title = title;
+    this.#priority = validatePriority(priority);
+}
+
+get added() {
+  return this.#added;
+}
+
+get title() {
+  return this.#title;
+}
+
+get priority() {
+  return this.#priority;
+}
+
 }
 
 
 class ToDo {
+  _tasks;
+
+  constructor() {
+      this._tasks = [];
+  }
     
 }
 
@@ -33,7 +65,7 @@ class ToDo {
 
 
 
-// Leave this code here for the automated tests
+// Code for the automated tests
 module.exports = {
   PRIORITY, validInteger, validatePriority, todaysDate, ToDo, Task,
 }
